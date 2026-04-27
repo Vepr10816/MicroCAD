2980195fe5a14842fbe6e5a881830de1f31a53f1
Vepr <vepritskiy28@gmail.com>
2026-04-27 22:16:29 +0300

    Update inheritance and constructor

--- include/Point.h
+++ include/Point.h
@@ -12,12 +12,20 @@ class Point : public Shape {
 
 public:
     Point(int id, double x, double y) : Shape(id), x_(x), y_(y) {}
+    Point(double x, double y) : x_(x), y_(y) {}
 
     std::string getInfo() const override {
         std::stringstream oss;
         oss << "Point [id=" << getId() << "]: (" << x_ << ", " << y_ << ")";
         return oss.str();
     }
+
+    double getArea() const override{
+        return 0;
+    }
+
+    double getX() const {return x_;}
+    double getY() const {return y_;}
 
 private:
     double x_;
--- include/Shape.h
+++ include/Shape.h
@@ -4,16 +4,20 @@
 
 /*
     Абстрактный класс Фигура
+    Конструктор по умолчанию - для точек, которые не являются фигурой. (Например точка входящая в линию)
 */
 
 class Shape {
 
 public:
     Shape(int id) : id_(id) {}
+    Shape() : id_(-1) {}
 
     virtual ~Shape() = default;
 
     virtual std::string getInfo() const = 0;
+
+    virtual double getArea() const = 0;
 
     int getId() const {
         return id_;
