3385e21fdbe2302522660671b910f974cd69a780
Vepr <vepritskiy28@gmail.com>
2026-04-27 22:18:50 +0300

    update main file with vector

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "Point.h"
+#include "Line.h"
+#include "Circle.h"
+
 
 int main() {
 
-    Point testPoint(1, 2.5, -1.3);
+    std::vector <std::unique_ptr<Shape>> shapes;
 
-    std::cout << testPoint.getInfo() << std::endl;
+    shapes.push_back(std::make_unique<Point>(1, 2.22, -1.2));
+    shapes.push_back(std::make_unique<Line>(2, 0, 2, 7, 9));
+    shapes.push_back(std::make_unique<Circle>(3, 0, 0, 25));
+
+    std::cout << "Количество фигур " << shapes.size() << ": " << std::endl << std::endl;
+
+    for(const auto& shape : shapes) {
+        std::cout << shape->getInfo() << ", Area = " << shape->getArea() << std::endl;
+    }
 
     return 0;
 }
