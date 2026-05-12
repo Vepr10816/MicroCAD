b9ea418d2a9199b958eed56e4dc1a5592d80b77b
Vepr <vepritskiy28@gmail.com>
2026-05-12 17:47:30 +0300

    Refactoring tabs

--- include/Circle.h
+++ include/Circle.h
@@ -13,36 +13,36 @@
 
 class Circle : public Shape
 {
-    public:
-        Circle(int id, double centreX, double centreY, double radius) : Shape(id), centre_(centreX, centreY), radius_(radius) {
-            if(radius_ <= 0)
-                radius_ = 0.001;
-        }
+public:
+    Circle(int id, double centreX, double centreY, double radius) : Shape(id), centre_(centreX, centreY), radius_(radius) {
+        if(radius_ <= 0)
+            radius_ = 0.001;
+    }
 
-        std::string getInfo() const override {
-            std::stringstream oss;
-            oss << "Circle [id="<<getId()<<"]: centre=("<<centre_.getX()<<", "<<centre_.getY()<<"), radius="<<radius_<<"";
-            return oss.str();
-        }
+    std::string getInfo() const override {
+        std::stringstream oss;
+        oss << "Circle [id="<<getId()<<"]: centre=("<<centre_.getX()<<", "<<centre_.getY()<<"), radius="<<radius_<<"";
+        return oss.str();
+    }
 
-        double getArea() const override {
-            return std::numbers::pi * (radius_ * radius_);
-        }
+    double getArea() const override {
+        return std::numbers::pi * (radius_ * radius_);
+    }
 
-        void move(double dx, double dy) override {
-            centre_.move(dx, dy);
-        }
+    void move(double dx, double dy) override {
+        centre_.move(dx, dy);
+    }
 
-        void scale(double factor) {
-            centre_.scale(factor);
-            radius_ *= factor;
-        }
+    void scale(double factor) {
+        centre_.scale(factor);
+        radius_ *= factor;
+    }
 
-        double getCenterX() const {return centre_.getX();}
-        double getCentreY() const {return centre_.getY();}
-        double getRadius() const {return radius_;}
+    double getCenterX() const {return centre_.getX();}
+    double getCentreY() const {return centre_.getY();}
+    double getRadius() const {return radius_;}
 
-    private:
-        Point centre_;
-        double radius_;
+private:
+    Point centre_;
+    double radius_;
 };
--- include/Line.h
+++ include/Line.h
@@ -11,35 +11,35 @@
 
 class Line : public Shape
 {
-    public:
-        Line(int id, double x1, double y1, double x2, double y2) : Shape(id), start_(x1, y1), end_(x2, y2) {}
+public:
+    Line(int id, double x1, double y1, double x2, double y2) : Shape(id), start_(x1, y1), end_(x2, y2) {}
         
-        std::string getInfo() const override{
-            std::stringstream oss;
-            oss << "Line [id="<<getId()<<"]: ("<<getStartX()<<", "<<getStartY()<<") -> ("<<getEndX()<<", "<<getEndY()<<")";
-            return oss.str();
-        }
+    std::string getInfo() const override{
+        std::stringstream oss;
+        oss << "Line [id="<<getId()<<"]: ("<<getStartX()<<", "<<getStartY()<<") -> ("<<getEndX()<<", "<<getEndY()<<")";
+        return oss.str();
+    }
 
-        double getArea() const override {
-            return 0;
-        }
+    double getArea() const override {
+        return 0;
+    }
 
-        void move(double dx, double dy) override {
-            start_.move(dx, dy);
-            end_.move(dx, dy);
-        }
+    void move(double dx, double dy) override {
+        start_.move(dx, dy);
+        end_.move(dx, dy);
+    }
 
-        void scale(double factor) override {
-            start_.scale(factor);
-            end_.scale(factor);
-        }
+    void scale(double factor) override {
+        start_.scale(factor);
+        end_.scale(factor);
+    }
 
-        double getStartX() const {return start_.getX();}
-        double getStartY() const {return start_.getY();}
-        double getEndX() const {return end_.getX();}
-        double getEndY() const {return end_.getY();}
+    double getStartX() const {return start_.getX();}
+    double getStartY() const {return start_.getY();}
+    double getEndX() const {return end_.getX();}
+    double getEndY() const {return end_.getY();}
 
-    private:
-        Point start_;
-        Point end_;
+private:
+    Point start_;
+    Point end_;
 };
